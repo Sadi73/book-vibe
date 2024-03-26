ca7bec9004ddb68e182785284df8fdfd905dd3ed
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-03-26 06:02:12 +0600

    books section added

---
+++ src/Components/Book.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StarIcon from '../assets/StarIcon.svg'
+
+const Book = ({ book }) => {
+    return (
+        <div className="card  bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={book?.image} alt="Shoes" className="w-full h-64 rounded-xl" />
+            </figure>
+            <div className="card-body ">
+                <div className='flex'>
+                    {book.tags.map((tag, index) => <p key={index}>{tag}</p>)}
+                </div>
+                <h2 className="card-title">{book?.bookName}</h2>
+                <p>By: {book?.author}</p>
+                <div className="card-actions border-t-2 border-dotted pt-3">
+                    <p>{book?.category}</p>
+                    <div className='flex gap-1'>
+                        <p>{book?.rating} </p>
+                        <img src={StarIcon} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
---
+++ src/Components/BooksSection.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Book from './Book';
+
+const BooksSection = () => {
+
+    const [allBooks, setAllBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('./books.json')
+            .then(res => res.json())
+            .then(data => setAllBooks(data))
+    }, []);
+
+    return (
+        <div>
+            <h1 className='text-5xl text-center'>Books</h1>
+
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {allBooks?.length > 0 &&
+                    allBooks.map(book => <Book
+                        key={book?.bookId}
+                        book={book} />)
+                }
+            </div>
+
+        </div>
+    );
+};
+
+export default BooksSection;
